79e94abfb64fdf8ef1fd363e2fd45ddf7f8ef945
Nico (Filipu) Guarino <nizoworld@gmail.com>
2021-09-26 16:33:31 -0700

    added player functionality

--- __tests__/random.test.js
+++
@@ -1,7 +0,0 @@
-const randomNumber = require('../lib/random');
-const Potion = require('../lib/Potion');
-
-test('gets random number between 1 and 10', () => {
-    expect(randomNumber()).toBeGreaterThanOrEqual(1);
-    expect(randomNumber()).toBeLessThanOrEqual(10);
-});
